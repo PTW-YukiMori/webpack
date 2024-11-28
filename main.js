f0f6e13eb4eeca72cc7898db6b40de569d253c16
PTW\yu_mori <yu_mori@ptw.inc>
2024-11-28 17:03:52 +0900

    適用

---
+++ main.js
@@ -0,0 +1,5 @@
+import { CreateMaterialDesignButton } from "./module/CreateMaterialDesignButton.mjs";
+
+const body = document.querySelector("body");
+
+CreateMaterialDesignButton('ボタン',() => console.log('hoge'));
